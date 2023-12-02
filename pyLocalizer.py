3624de6c37988bc173814eb4569e836e888255d2
axkralj990 <alex.kraljic@gmail.com>
2023-12-02 23:21:43 +0100

    add colored labeling and rotation function (test)

--- pyLocalizer.py
+++ pyLocalizer.py
@@ -21,6 +21,14 @@ class PandasModel(QAbstractTableModel):
     def __init__(self, data):
         super().__init__()
         self._data = data
+        self._plotter = None
+        self._labels = []
+
+    def set_plotter(self, plotter):
+        self._plotter = plotter
+        
+    def set_labels(self, labels):
+        self._labels = labels        
 
     def rowCount(self, parent=QModelIndex()):
         return self._data.shape[0]
@@ -69,28 +77,61 @@ class PandasModel(QAbstractTableModel):
         else:    
             return str(np.max(numeric_ids) + 1)
     
-    def render(self, plotter: vd.Plotter):
-        pts = []
-        ic(self._data.shape)
-        for row in self._data.iterrows():
-            ic(row)
-            pts.append([row[1]['x'], row[1]['y'], row[1]['z']])
-            
-        for actor in plotter.actors:
-            if isinstance(actor, vd.Spheres):
-                plotter.remove(actor)
+    def render(self) -> None:
+        if self._plotter is None:
+            raise ValueError('Plotter not set')
         
-        if len(pts) > 0:
-            spheres = vd.Spheres(pts, r=0.005, res=8, c='r5', alpha=1)
-            plotter.add(spheres).render()
-        else:
-            plotter.render()
+        points_unlabeled = []
+        points_labeled = []
+        for row in self._data.iterrows():
+            if row[1]['label'] in self._labels:
+                points_labeled.append([row[1]['x'], row[1]['y'], row[1]['z']])
+            else:
+                points_unlabeled.append([row[1]['x'], row[1]['y'], row[1]['z']])
+        
+        # remove all actors except the mesh and the text
+        # for actor in self._plotter.actors:
+        #     if isinstance(actor, vd.Mesh) == False and isinstance(actor, vd.Text2D) == False:
+        #         self._plotter.remove(actor)
+        
+        if len(self._plotter.actors) > 2:
+            self._plotter.remove(self._plotter.actors[2:])
+        
+        if len(points_unlabeled) > 0:
+            spheres = vd.Spheres(points_unlabeled, r=0.004, res=8, c='r5', alpha=1)
+            self._plotter.add(spheres)
+        
+        if len(points_labeled) > 0:
+            spheres = vd.Spheres(points_labeled, r=0.004, res=8, c='b5', alpha=1)
+            self._plotter.add(spheres)
+            
+        self._plotter.render()
             
         self.layoutChanged.emit()
+        
+    # def dataChanged(self, index, index2, roles):
+    #     ic("Data changed")
+        # self.layoutChanged.emit()
+        # self.render()
+    #     return True
+    
+    def rotate_xyz_of_data_by_x_degrees(self, degrees):
+        # extract xyz from _data
+        xyz = self._data[['x', 'y', 'z']].values
+        # create a rotation matrix (do not use vd)
+        theta = np.radians(degrees)
+        c, s = np.cos(theta), np.sin(theta)
+        R = np.array(((1, 0, 0), (0, c, -s), (0, s, c)))
+        # rotate xyz
+        xyz_rotated = np.dot(xyz, R)
+        # update _data
+        self._data[['x', 'y', 'z']] = xyz_rotated
 
     def setData(self, index, value, role):
+        id("setData")
         if role == Qt.ItemDataRole.EditRole:
             self._data.iloc[index.row(), index.column()] = value
+            self.render()
             return True
         return False
 
@@ -115,10 +156,10 @@ class StartQt6(QtWidgets.QMainWindow):
 
         # data = pd.DataFrame([[1, 9, 2], [1, 0, -1], [3, 5, 2], [3, 3, 2], [5, 8, 9],], columns=["A", "B", "C"])
         # create an empyt dataframe with columns ID, x, y, z, modality
-        data = pd.DataFrame(columns=["ID", "x", "y", "z", "modality", "labeled"])
+        data = pd.DataFrame(columns=["ID", "x", "y", "z", "modality", "label"])
         self.model = PandasModel(data)
         self.ui.electrodes_table.setModel(self.model)
-        for column_hidden in (5,):
+        for column_hidden in (1, 2, 3):
             self.ui.electrodes_table.hideColumn(column_hidden)
 
         # prepare vedo canvas
@@ -148,6 +189,8 @@ class StartQt6(QtWidgets.QMainWindow):
         
         self.ui.process_table_button.clicked.connect(self.process_table)
         
+        # self.model.dataChanged.connect(self.model.render)
+        
         self.image = None
         self.dog = None
         
@@ -165,10 +208,7 @@ class StartQt6(QtWidgets.QMainWindow):
         self.ui.statusbar.showMessage('Welcome!')
 
     def process_table(self):
-        # append a new random row to the table
-        new_row = pd.DataFrame([[np.random.randint(0, 10), np.random.randint(0, 10), np.random.randint(0, 10)]], columns=["A", "B", "C"])
-        self.model._data = pd.concat([self.model._data, new_row])
-        self.model.layoutChanged.emit()
+        self.model.rotate_xyz_of_data_by_x_degrees(10)
         
     # define slots (functions)
     def load_surface(self):
@@ -201,8 +241,11 @@ class StartQt6(QtWidgets.QMainWindow):
             
             self.plt.show(self.mesh, msg, __doc__)
             
-            if spheres is not None:
-                self.plt.add(spheres).render()
+            self.model.set_plotter(self.plt)
+            self.model.set_labels(['2', '6', '8'])
+            
+            # if spheres is not None:
+            #     self.plt.add(spheres).render()
 
     def onMouseClick(self, evt):
         vd.printc("You have clicked your mouse button. Event info:\n", evt, c='y')
@@ -234,13 +277,23 @@ class StartQt6(QtWidgets.QMainWindow):
         print("left click")
         pt = evt.picked3d
         
+        id = self.model.get_next_id()
+        
         if pt is not None:  
-            new_row = pd.DataFrame([[self.model.get_next_id(),
-                                     pt[0], pt[1], pt[2],
-                                     "scan", False]],
-                                   columns=["ID", "x", "y", "z" , "modality", "labeled"])
+            # new_row = pd.DataFrame([[self.model.get_next_id(),
+            #                             pt[0], pt[1], pt[2],
+            #                             "scan", False]],
+            #                         columns=["ID", "x", "y", "z" , "modality", "label"])
+            new_row = pd.DataFrame({
+                "ID": id,
+                "x": pt[0],
+                "y": pt[1],
+                "z": pt[2],
+                "modality": "scan",
+                "label": id
+            }, index=[0])
             self.model.insert_point(new_row)
-            self.model.render(self.plt)
+            self.model.render()
 
     def on_right_click(self, evt):
         print("right click")
@@ -248,7 +301,7 @@ class StartQt6(QtWidgets.QMainWindow):
         
         if pt is not None:
             self.model.remove_closest_point(pt)
-            self.model.render(self.plt)
+            self.model.render()
             
     # def on_left_click(self, evt):
     #     print("left click")

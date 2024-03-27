bee6a58aa2b20cec4bd19c630e6ee6ae00c8197e
axkralj990 <alex.kraljic@gmail.com>
2024-03-27 16:20:38 +0100

    feat: split surfaceview to two children classes, interactive view and labeling view

--- ui/surface_view.py
+++ ui/surface_view.py
@@ -1,5 +1,4 @@
 from PyQt6.QtWidgets import QAbstractItemView 
-from typing import Optional
 
 try:
     from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
@@ -12,18 +11,23 @@ import vedo as vd
 from core.electrode import Electrode
 from core.cap_model import CapModel
 
-import numpy as np
+import numpy as np   
+
+from config.colors import (HEADSCAN_LABELED_ELECTRODES_COLOR,
+                           HEADSCAN_UNLABELED_ELECTRODES_COLOR, LABELING_REFERENCE_ELECTRODES_COLOR,
+                           MRI_LABELED_ELECTRODES_COLOR,
+                           MRI_UNLABELED_ELECTRODES_COLOR,
+                           LABELING_MEASURED_UNLABELED_ELECTRODES_COLOR,
+                           LABELING_MEASURED_LABELED_ELECTRODES_COLOR)
 
 class SurfaceView(QAbstractItemView):
     """SurfaceView class for displaying a 3D surface in a Qt application."""
-    def __init__(self, frame, mesh: Optional[vd.Mesh] = None,
+    def __init__(self, frame, mesh: vd.Mesh | None = None,
                  modality: str = "", config = {}, parent=None):
         super().__init__(parent)
         self._vtk_widget = QVTKRenderWindowInteractor(frame)
         
         self._plotter = vd.Plotter(qt_widget=self._vtk_widget, axes=2)
-        self._plotter.add_callback('LeftButtonPress', self._on_left_click)
-        self._plotter.add_callback('RightButtonPress', self._on_right_click)
         
         self.mesh = mesh
         
@@ -55,98 +59,16 @@ class SurfaceView(QAbstractItemView):
         self.render_electrodes()
         
     def render_electrodes(self):
-        # start_electrodes_idx = 1
-        # if self.secondary_mesh is not None:
-        #     start_electrodes_idx = 2
-        
-        # if len(self._plotter.actors) > start_electrodes_idx:
-        #     self._plotter.remove(self._plotter.actors[start_electrodes_idx:])
-        
-        self._plotter.clear()
-        self._plotter.add(self.mesh)
-        if self.secondary_mesh is not None:
-            self._plotter.add(self.secondary_mesh)
-        
-        points_unlabeled = []
-        points_labeled = []
-        for i in range(self.model.rowCount()):
-            electrode_modality = self.model.get_electrode(i).modality
-            
-            if electrode_modality != self.modality and self.modality != "both":
-                continue
-            
-            point = self.model.get_electrode(i).coordinates
-            label = self.model.get_electrode(i).label
-            
-            if electrode_modality == "mri":
-                unlabeled_color = "r8"
-                labeled_color = "b8"
-            else:
-                unlabeled_color = "r5"
-                labeled_color = "b5"
-            
-            if label is None or label == "" or label == "None":
-                self.model.set_electrode_labeled_flag(i, False)
-                label = str(i+1)
-                points_unlabeled.append((point, label, unlabeled_color))
-            else:
-                self.model.set_electrode_labeled_flag(i, True)
-                points_labeled.append((point, label, labeled_color))
-            
-        for point, label, color in points_unlabeled:
-            sphere = vd.Sphere(
-                point,
-                r=self.config["sphere_size"],
-                res=8, c=color, alpha=1) # type: ignore
-            self._plotter.add(sphere)
-            if self.config["draw_flagposts"]:
-                fs = self.get_flagpost(
-                    label, point, height=self.config["flagpost_height"],
-                    size=self.config["flagpost_size"],
-                    color=color) # type: ignore
-                self._plotter.add(fs)
-        
-        for point, label, color in points_labeled:
-            sphere = vd.Sphere(
-                point,
-                r=self.config["sphere_size"],
-                res=8, c=color, alpha=1) # type: ignore
-            self._plotter.add(sphere)
-            if self.config["draw_flagposts"]:
-                fs = self.get_flagpost(
-                    label, point,
-                    height=self.config["flagpost_height"],
-                    size=self.config["flagpost_size"],
-                    color=color) # type: ignore
-                self._plotter.add(fs)
-            
-        self._plotter.render()
+        pass
         
     def get_flagpost(self, text, point: np.ndarray, height: float = 0.005,
-                     size: float = 0.2, color: str ='r5') -> vd.Flagpost:
+                     size: float = 0.2,
+                     color: str | tuple[int, int, int] ='r5',
+                     background_color: str | tuple[int, int, int] = 'k9') -> vd.Flagpost:
         top_point = ((point-self.mesh_centroid) /
                      np.linalg.norm(point-self.mesh_centroid))*height+point
-        fs = vd.Flagpost(text, base=point, top=top_point, c=color, s=size)       # type: ignore
+        fs = vd.Flagpost(text, base=point, top=top_point, c=color, bc=background_color, s=size)       # type: ignore
         return fs
-        
-    def _on_left_click(self, evt):
-        point = evt.picked3d
-        if point is not None:
-            electrode = Electrode(point,
-                                modality=self.modality,
-                                label="None")
-            self.model.insert_electrode(electrode)
-            
-            self.render_electrodes()
-            self.model.layoutChanged.emit()
-        
-    def _on_right_click(self, evt):
-        point = evt.picked3d
-        if point is not None:
-            self.model.remove_closest_electrode(point, self.modality)
-            
-            self.render_electrodes()
-            self.model.layoutChanged.emit()
         
     def dataChanged(self, topLeft, bottomRight, roles):
         self.render_electrodes()
@@ -187,3 +109,156 @@ class SurfaceView(QAbstractItemView):
     
     def close_vtk_widget(self):
         self._vtk_widget.close()
+
+
+class InteractiveSurfaceView(SurfaceView):
+    def __init__(self, frame, mesh: vd.Mesh | None = None,
+                 modality: str = "", config = {}, parent=None):
+        super().__init__(frame, mesh, modality, config, parent)
+        
+        self._plotter.add_callback('LeftButtonPress', self._on_left_click)
+        self._plotter.add_callback('RightButtonPress', self._on_right_click)
+        
+    def render_electrodes(self):
+        self._plotter.clear()
+        self._plotter.add(self.mesh)
+        if self.secondary_mesh is not None:
+            self._plotter.add(self.secondary_mesh)
+        
+        points_unlabeled = []
+        points_labeled = []
+        for i in range(self.model.rowCount()):
+            electrode_modality = self.model.get_electrode(i).modality
+            
+            if electrode_modality != self.modality and self.modality != "both":
+                continue
+            
+            point = self.model.get_electrode(i).coordinates
+            label = self.model.get_electrode(i).label
+            
+            if electrode_modality == "mri":
+                unlabeled_color = MRI_UNLABELED_ELECTRODES_COLOR
+                labeled_color = MRI_LABELED_ELECTRODES_COLOR
+            else:
+                unlabeled_color = HEADSCAN_UNLABELED_ELECTRODES_COLOR
+                labeled_color = HEADSCAN_LABELED_ELECTRODES_COLOR
+            
+            if label is None or label == "" or label == "None":
+                #self.model.set_electrode_labeled_flag(i, False)
+                label = str(i+1)
+                points_unlabeled.append((point, label, unlabeled_color))
+            else:
+                #self.model.set_electrode_labeled_flag(i, True)
+                points_labeled.append((point, label, labeled_color))
+            
+        for point, label, color in points_unlabeled:
+            sphere = vd.Sphere(
+                point,
+                r=self.config["sphere_size"],
+                res=8, c=color, alpha=1) # type: ignore
+            self._plotter.add(sphere)
+            if self.config["draw_flagposts"]:
+                fs = self.get_flagpost(
+                    label, point, height=self.config["flagpost_height"],
+                    size=self.config["flagpost_size"],
+                    color=color) # type: ignore
+                self._plotter.add(fs)
+        
+        for point, label, color in points_labeled:
+            sphere = vd.Sphere(
+                point,
+                r=self.config["sphere_size"],
+                res=8, c=color, alpha=1) # type: ignore
+            self._plotter.add(sphere)
+            if self.config["draw_flagposts"]:
+                fs = self.get_flagpost(
+                    label, point,
+                    height=self.config["flagpost_height"],
+                    size=self.config["flagpost_size"],
+                    color=color) # type: ignore
+                self._plotter.add(fs)
+            
+        self._plotter.render()
+        
+    def _on_left_click(self, evt):
+        point = evt.picked3d
+        if point is not None:
+            electrode = Electrode(point,
+                                modality=self.modality,
+                                label="None")
+            self.model.insert_electrode(electrode)
+            
+            self.render_electrodes()
+            self.model.layoutChanged.emit()
+        
+    def _on_right_click(self, evt):
+        point = evt.picked3d
+        if point is not None:
+            self.model.remove_closest_electrode(point, self.modality)
+            
+            self.render_electrodes()
+            self.model.layoutChanged.emit()  
+
+class LabelingSurfaceView(SurfaceView):
+    def __init__(self, frame, mesh: vd.Mesh | None = None,
+                 modality: str = "", config = {}, parent=None):
+        super().__init__(frame, mesh, modality, config, parent)
+        
+    def render_electrodes(self):
+        
+        self._plotter.clear()
+        self._plotter.add(self.mesh)
+        if self.secondary_mesh is not None:
+            self._plotter.add(self.secondary_mesh)
+        
+        points_unlabeled = []
+        points_labeled = []
+        for i in range(self.model.rowCount()):
+            electrode_modality = self.model.get_electrode(i).modality
+            
+            point = self.model.get_electrode(i).unit_sphere_cartesian_coordinates
+            label = self.model.get_electrode(i).label
+            
+            if electrode_modality == "reference":
+                labeled_color = LABELING_REFERENCE_ELECTRODES_COLOR
+                unlabeled_color = '#000000'
+            else:
+                unlabeled_color = LABELING_MEASURED_UNLABELED_ELECTRODES_COLOR
+                labeled_color = LABELING_MEASURED_LABELED_ELECTRODES_COLOR
+            
+            if label is None or label == "" or label == "None":
+                # self.model.set_electrode_labeled_flag(i, False)
+                label = str(i+1)
+                points_unlabeled.append((point, label, unlabeled_color))
+            else:
+                # self.model.set_electrode_labeled_flag(i, True)
+                points_labeled.append((point, label, labeled_color))
+            
+        for point, label, color in points_unlabeled:
+            sphere = vd.Sphere(
+                point,
+                r=self.config["sphere_size"],
+                res=8, c=color, alpha=1) # type: ignore
+            self._plotter.add(sphere)
+            if self.config["draw_flagposts"]:
+                fs = self.get_flagpost(
+                    label, point, height=self.config["flagpost_height"],
+                    size=self.config["flagpost_size"],
+                    color=color) # type: ignore
+                self._plotter.add(fs)
+        
+        for point, label, color in points_labeled:
+            sphere = vd.Sphere(
+                point,
+                r=self.config["sphere_size"],
+                res=8, c=color, alpha=1) # type: ignore
+            self._plotter.add(sphere)
+            if self.config["draw_flagposts"]:
+                fs = self.get_flagpost(
+                    label, point,
+                    height=self.config["flagpost_height"],
+                    size=self.config["flagpost_size"],
+                    color=color) # type: ignore
+                self._plotter.add(fs)
+            
+        self._plotter.render()

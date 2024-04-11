944753d9f339007154ea53b3814d6a296ad59cf3
axkralj990 <alex.kraljic@gmail.com>
2024-04-11 11:55:07 +0200

    Fix: Improve the default texture circle detection values

--- config/electrode_detector.py
+++ config/electrode_detector.py
@@ -2,15 +2,17 @@
 Electrode detector configuration file
 """
 
+
 class DogParameters:
-    KSIZE = 35
-    SIGMA = 12
+    KSIZE = 39
+    SIGMA = 16
     FACTOR = 1.1
     THRESHOLD_LEVEL = 1
-    
+
+
 class HoughParameters:
     PARAM1 = 5
-    PARAM2 = 12
+    PARAM2 = 7.5
     MIN_DISTANCE = 100
     MIN_RADIUS = 12
     MAX_RADIUS = 20

8dd4be1e40740f97bfd65639f2927abf6584edd8
axkralj990 <alex.kraljic@gmail.com>
2024-04-11 11:54:37 +0200

    Chore: Move loader and exporter to a new "data" directory

---
+++ data/exporter.py
@@ -0,0 +1,26 @@
+from model.electrode import Electrode
+from config.mappings import ElectrodesFileMapping
+
+
+def export_electrodes_to_file(electrodes: list[Electrode], filename: str) -> None:
+    with open(filename, "w") as f:
+        # write the header
+        f.write(
+            (
+                f"{ElectrodesFileMapping.CED_LABEL}\t"
+                f"{ElectrodesFileMapping.CED_X}\t"
+                f"{ElectrodesFileMapping.CED_Y}\t"
+                f"{ElectrodesFileMapping.CED_Z}\n"
+            )
+        )
+
+        # write the electrodes
+        for electrode in electrodes:
+            f.write(
+                (
+                    f"{electrode.label}\t"
+                    f"{electrode.coordinates[0]}\t"
+                    f"{electrode.coordinates[1]}\t"
+                    f"{electrode.coordinates[2]}\n"
+                )
+            )
